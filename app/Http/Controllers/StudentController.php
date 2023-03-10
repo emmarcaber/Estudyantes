d3e7da2ef136d454c59c16b4742a01d495fd90e2
Emmar Caber <caberemmar@gmail.com>
2023-03-10 16:03:59 +0800

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     // Show all students
     public function index() {
-        return view('students.index');
+        return view('students.index' , [
+            'students' => Student::latest()->get()
+        ]);
     }
 }
