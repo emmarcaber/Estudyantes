5b445493cd90a7a68b9ea2c7831a26102a1f08f2
Emmar Caber <caberemmar@gmail.com>
2023-03-10 17:15:10 +0800

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -10,7 +10,15 @@ class StudentController extends Controller
     // Show all students
     public function index() {
         return view('students.index' , [
+            'heading' => 'ESTUDYANTES',
             'students' => Student::latest()->get()
+        ]);
+    }
+
+    // Show create student form
+    public function create() {
+        return view('students.create', [
+            'heading' => 'Create Estudyante'
         ]);
     }
 }
