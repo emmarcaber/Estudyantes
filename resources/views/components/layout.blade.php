efd294fe629377ad4400ef13e5f1ec124abd1b81
Emmar Caber <caberemmar@gmail.com>
2023-03-10 15:32:18 +0800

    Update components/layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,10 +8,16 @@
     <title></title>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
 </head>
+
 <body>
-    
+
+    <div class="container mt-3">
+        <h1 class="text-center">STUDENTS</h1>
+    </div>
+
     {{ $slot }}
 
     <script src="{{ asset('js/script.js') }}"></script>
 </body>
+
 </html>
