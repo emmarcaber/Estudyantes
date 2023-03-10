764caf91bae36cbd283fc628c2998118cb818c81
Emmar Caber <caberemmar@gmail.com>
2023-03-10 17:39:18 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function() {
-    return 'I am running...';
-});
+Route::get('/', [StudentController::class, 'index'])->name('students.index');
 
-Route::get('/students', [StudentController::class, 'index'])->name('students.index');
 Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+
+Route::post('/students', [StudentController::class, 'store'])->name('students.store');
