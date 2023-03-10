bfaac4fa2a2f3e71eb0689f5c15ab4600d72fe49
Emmar Caber <caberemmar@gmail.com>
2023-03-10 16:19:18 +0800

    Update components/layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ESTUDYANTES</title>
+    <title>Student</title>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
