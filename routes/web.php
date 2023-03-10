51c52e4d1fb3b27bfde1aea4243c47d71bca8bcc
Emmar Caber <caberemmar@gmail.com>
2023-03-10 18:16:04 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/', [StudentController::class, 'index'])->name('students.index');
 Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
 
 Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+
+Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name('students.edit');
