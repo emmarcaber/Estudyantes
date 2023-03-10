7a0711edf6d73d33fa09727cb4943150a6e02451
Emmar Caber <caberemmar@gmail.com>
2023-03-10 17:16:55 +0800

    Update students/create.blade.php

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,10 +1,12 @@
-<x-layout>
+<x-layout :heading="$heading">
     <div class="row">
         <div class="text-end">
-            <button class="btn btn-success">
-                <i class="fas fa-plus"></i>
-                Add Student
-            </button>
+            <a href="{{ route('students.create') }}">
+                <button class="btn btn-success">
+                    <i class="fas fa-plus"></i>
+                    Add Student
+                </button>
+            </a>
         </div>
 
         <table class="table table-bordered text-center mt-3">
@@ -18,25 +20,25 @@
                 </tr>
 
                 @foreach ($students as $student)
-                <tr>
-                    <td class="text-start">{{ $student->name }}</td>
-                    <td>{{ $student->course }}</td>
-                    <td>{{ $student->email }}</td>
-                    <td>{{ $student->gender }}</td>
-                    <td>
-                        <button class="btn btn-warning">
-                            <i class="fas fa-edit"></i>
-                            Edit
-                        </button>
-                        <button class="btn btn-danger">
-                            <i class="fas fa-trash"></i>
-                            Delete
-                        </button>
-                    </td>
-                </tr>
+                    <tr>
+                        <td class="text-start">{{ $student->name }}</td>
+                        <td>{{ $student->course }}</td>
+                        <td>{{ $student->email }}</td>
+                        <td>{{ $student->gender }}</td>
+                        <td>
+                            <button class="btn btn-warning">
+                                <i class="fas fa-edit"></i>
+                                Edit
+                            </button>
+                            <button class="btn btn-danger">
+                                <i class="fas fa-trash"></i>
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
                 @endforeach
             </thead>
         </table>
     </div>
-    
+
 </x-layout>
