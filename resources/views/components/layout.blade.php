e46559062c203ecc47540ff2a666ad9338c4af0e
Emmar Caber <caberemmar@gmail.com>
2023-03-10 17:16:16 +0800

    Update components/layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,3 +1,4 @@
+@props(['heading'])
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ESTUDYANTES</title>
+    <title>{{ $heading }}</title>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
@@ -13,7 +14,7 @@
 <body>
 
     <div class="container mt-3">
-        <h1 class="text-center">ESTUDYANTES</h1>
+        <h1 class="text-center">{{ $heading }}</h1>
 
         {{ $slot }}
     </div>
