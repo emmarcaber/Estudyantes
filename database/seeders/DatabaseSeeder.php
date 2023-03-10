05bcdc315ffb68d6557644ad9b6626a8e1578eb6
Emmar Caber <caberemmar@gmail.com>
2023-03-10 15:47:30 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Student;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+
+        Student::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
